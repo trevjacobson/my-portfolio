47b6406900a05dd3379c4cd3f04f20ad4421c98a
Noahmay Pelletier <pellen1@unlv.nevada.edu>
2019-12-02 20:08:11 -0800

    Updates

---
+++ precache-manifest.41c6eda11c0aa0122ebc6aee5db22c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d70b7cc88509d17ab1599006873182b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d059f9412463194df31",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c8ac8471a641051ab4f0",
+    "url": "/static/js/2.c5f5b089.chunk.js"
+  },
+  {
+    "revision": "1d059f9412463194df31",
+    "url": "/static/js/main.62c720de.chunk.js"
+  },
+  {
+    "revision": "6fec38a83bf67aca18f4",
+    "url": "/static/js/runtime-main.fb3cf872.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
